9a55390b3ced4f7b49ef00b5514e0d968862fcc9
Jeff Lieu <jefflieu@gmail.com>
2018-07-23 16:23:03 -0700

    Created navbar

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,11 @@ import '../assets/css/app.css';
 import {Route} from 'react-router-dom';
 import ChatRoom from './chat_room';
 import Home from './home';
+import Nav from './nav';
 
 const App = () => (
     <div>
+        <Nav/>
         <div className="container">
             <Route exact path="/" component={Home}/>
             <Route path="/chat-room" component={ChatRoom}/>
