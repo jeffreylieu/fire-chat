2e8d0fd305a232341568671613250ff5f12dabe6
Jeff Lieu <jefflieu@gmail.com>
2018-09-24 16:03:41 -0700

    fixed user colors and added auto delete

--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from './middleware/thunk';
 import rootReducer from './reducers';
 import App from './components/app';
+import types from './actions/types';
 
 const store = createStore(rootReducer, {}, applyMiddleware(thunk));
+const user = localStorage.getItem('user');
+
+if(user){
+    const payload = JSON.parse(user);
+
+    store.dispatch({
+        type: types.SET_USER_DATA,
+        payload: user
+    });
+}
 
 ReactDOM.render(
     <Provider store={store}>
